ec089b7558ffd2f442c22685f8c018a52a4392fa
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2020-06-22 11:01:50 -0400

    Started controlling the plunger

---
+++ Pinball/Assets/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+	#region Fields
+
+	public Plunger Plunger;
+	
+	#endregion
+
+	// Start is called before the first frame update
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		HandleFlippers();
+
+	}
+
+	private void HandleFlippers()
+	{
+		throw new NotImplementedException();
+	}
+
+
+
+
+
+}
---
+++ Pinball/Assets/Plunger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plunger : MonoBehaviour
+{
+
+	public float PullSpeed;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		HandlePlunger();
+
+	}
+
+	private void HandlePlunger()
+	{
+
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+
+			transform.position -= transform.up * PullSpeed * Time.deltaTime;
+
+		}
+
+
+	}
+
+}
